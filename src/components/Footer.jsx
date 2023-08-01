19dd1e2deb8c9adae17e670af21bdbc1f35fc609
Tobin Waters <tkwaterss@gmail.com>
2023-07-31 19:18:11 -0600

    started working on contact form inputs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,8 @@ const footer = () => {
   //Social Links Here
   return (
     <LeftSection className={classes.footerSection}>
-      <h3>Send me a message!</h3>
+      <div className={classes.footerContent}>
+        {/* <h3>Send me a message!</h3>
       <textarea></textarea>
       <button>Send</button>
       <h4>Or email me: tkwaterss@gmail.com</h4>
@@ -17,6 +18,38 @@ const footer = () => {
         <li>Personal Portfolio</li>
         <li>Github Account</li>
       </ul>
+       */}
+
+        <div className={classes.row}>
+          <span>
+            <input
+              className={classes.balloon}
+              id="state"
+              type="text"
+              placeholder="Liquid, solid, gaseous..."
+            />
+            <label for="state">State</label>
+          </span>
+          <span>
+            <input
+              className={classes.balloon}
+              id="planet"
+              type="text"
+              placeholder="Probably Earth"
+            />
+            <label for="planet">Planet</label>
+          </span>
+          <span>
+            <input
+              className={classes.balloon}
+              id="galaxy"
+              type="text"
+              placeholder="Milky Way?"
+            />
+            <label for="galaxy">Galaxy</label>
+          </span>
+        </div>
+      </div>
     </LeftSection>
   );
 };
